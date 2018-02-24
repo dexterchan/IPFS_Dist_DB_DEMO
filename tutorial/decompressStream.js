dc0ce47b1db06410cf28de2c753ad71619ce8636
dexterchan <dexterchan@gmail.com>
2018-02-24 23:48:29 +0800

    tutorial2

--- tutorial/decompressStream.js
+++ tutorial/decompressStream.js
@@ -2,8 +2,12 @@ var fs = require("fs");
 var zlib = require('zlib');
 
 // Decompress the file input.txt.gz to input.txt
-fs.createReadStream('/Users/dexter/Documents/IPFS_Dist_DB_DEMO/tutorial/input.txt.gz')
+inputfile=__dirname + "/input.txt.gz"
+outputfile=__dirname + "/input2.txt"
+fs.createReadStream(inputfile)
    .pipe(zlib.createGunzip())
-   .pipe(fs.createWriteStream('/Users/dexter/Documents/IPFS_Dist_DB_DEMO/tutorial/input.txt'));
+   .pipe(fs.createWriteStream(outputfile));
   
 console.log("File Decompressed.");
+
+console.log( __dirname );
