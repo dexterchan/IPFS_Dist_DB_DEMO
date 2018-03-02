0714de78e3b0d2f6ea294711db284877a956b291
dexterchan <dexterchan@gmail.com>
2018-03-02 16:48:13 +0800

    data model

---
+++ static/TradeInfo.js
@@ -0,0 +1,13 @@
+TradeInfo={
+   Location1="A",
+   Location2="B",
+    party1="A1",
+    party2="B1",
+    TradeId="000000",
+    TradeVersion=1,
+    TimeStamp="20180101 00:00:00:00",
+    Direction="sell",
+    Security="ABC",
+    quantity=1000000000,
+    price=90.00
+}
---
+++ static/ipfsTradeIndex.js
@@ -0,0 +1,12 @@
+IpfsTradeIndex={
+    //Key
+    Location1="A",
+   Location2="B",
+    TradeId="000000",
+    TradeVersion=1,
+    TimeStamp="20180101 00:00:00:00",
+
+    //Value
+    urlLink1="encrypted link by location1",
+    urllink2="encrypted link by location2"
+}
